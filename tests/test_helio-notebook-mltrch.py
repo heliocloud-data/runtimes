2c59cd5549fed2efd1adabd290fc59919a6aa520
Container Developer <brian.a.thomas@nasa.gov>
2023-08-04 14:13:23 +0000

    add helio notebook for torch

---
+++ tests/test_helio-notebook-mltrch.py
@@ -0,0 +1,19 @@
+import pytest
+import importlib
+import sys
+import os
+
+packages = [
+    # key packages
+    'torch',  #'torchvision', 
+    'cv2'
+    ]
+
+@pytest.mark.parametrize('package_name', packages, ids=packages)
+def test_import(package_name):
+    importlib.import_module(package_name)
+    
+def test_start():
+    print(os.environ)
+    if os.environ.get('HELIOCLOUD_ENV') is not None:
+        assert os.environ['HELIOCLOUD_ENV'] == 'helio-notebook-mltrch'
